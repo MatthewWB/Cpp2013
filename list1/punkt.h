28f00f3055a8d3a7e6d80402a92389a362db3521
wrobel93b <ptaszynka93b@gmail.com>
2013-04-17 12:57:55 +0300

    Create punkt.h

---
+++ list1/punkt.h
@@ -0,0 +1,12 @@
+#ifndef PUNKT_H_INCLUDED
+#define PUNKT_H_INCLUDED
+
+class punkt {
+public:
+    double x, y;
+    void przesun(double a, double b);
+
+    punkt(double a, double b);
+};
+
+#endif // PUNKT_H_INCLUDED
