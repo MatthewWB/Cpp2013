e092b399d8bc5caa7a0c7866a47504d974688306
wrobel93b <ptaszynka93b@gmail.com>
2013-04-17 12:53:01 +0300

    Create headers.h

---
+++ list1/headers.h
@@ -0,0 +1,7 @@
+#ifndef HEADERS_H_INCLUDED
+#define HEADERS_H_INCLUDED
+
+bool punkt_wspolny(kolo klasa, punkt pkt);
+bool punkty_wspolne(kolo pierwsze, kolo drugie);
+
+#endif // HEADERS_H_INCLUDED
